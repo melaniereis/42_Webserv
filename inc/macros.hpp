ba9e91c5ba0b90dec086e94d25d8879678a81129
m3irel3s <miguelmeireles025@gmail.com>
2025-06-02 14:18:08 +0100

    Added more structure to the project, config and pages folders

--- inc/macros.hpp
+++ inc/macros.hpp
@@ -1,18 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   macros.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jmeirele <jmeirele@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/06/02 13:21:37 by jmeirele          #+#    #+#             */
-/*   Updated: 2025/06/02 14:15:59 by jmeirele         ###   ########.fr       */
+/*   Created: 2025/06/02 14:09:05 by jmeirele          #+#    #+#             */
+/*   Updated: 2025/06/02 14:16:01 by jmeirele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "inc/webserv.hpp"
+#pragma once
 
-int main(void)
-{
-	std::cout << "Hello from webserv" << std::endl;
-}
--- inc/webserv.hpp
+++ inc/webserv.hpp
@@ -6,12 +6,37 @@
 /*   By: jmeirele <jmeirele@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/02 13:18:32 by jmeirele          #+#    #+#             */
-/*   Updated: 2025/06/02 13:26:19 by jmeirele         ###   ########.fr       */
+/*   Updated: 2025/06/02 14:15:57 by jmeirele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
-// CPP INCLUDES
+// C++ Standard
 #include <iostream>
-#include <string.h>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+
+// Networking / Sockets
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+// Filesystem & CGI
+#include <sys/stat.h>
+#include <dirent.h>
+#include <signal.h>
+#include <sys/wait.h>
+
+// Error Handling
+#include <cerrno>
+#include <cstring>
+
+// Includes 
+#include "macros.hpp"
