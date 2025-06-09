9b99c98eba8d9774b90e4d0329b5e6066529406c
m3irel3s <miguelmeireles025@gmail.com>
2025-06-09 20:50:39 +0100

    Get implemented, default behaviour for returning html pages

--- src/http/RequestHandler.cpp
+++ src/http/RequestHandler.cpp
@@ -6,7 +6,7 @@
 /*   By: jmeirele <jmeirele@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/09 18:31:25 by jmeirele          #+#    #+#             */
-/*   Updated: 2025/06/09 18:48:20 by jmeirele         ###   ########.fr       */
+/*   Updated: 2025/06/09 20:47:12 by jmeirele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,9 +30,40 @@ Response RequestHandler::handle(const Request &request)
 
 Response RequestHandler::handleGetMethod(const Request &request)
 {
-	(void)request;
-	Response res;
-	return res;
+	Response response;
+	std::string path = request.getReqPath();
+
+	if (path == "/")
+		path = "index.html";
+	
+	std::string fullPath = "pages/" + path;
+	std::ifstream file(fullPath.c_str());
+	
+	// Prevent directory traversal
+	if (path.find("..") != std::string::npos)
+	{
+		response.setStatus(403, "Forbidden");
+		response.setBody("<h1>403 Forbidden</h1>");
+		response.setHeader("Content-Type", "text/html");
+		return response;
+	}
+
+	if (!file)
+	{
+		response.setStatus(404, "Not Found");
+		response.setBody("<h1>404 Not Found</h1>");
+		response.setHeader("Content-Type", "text/html");
+		return response;
+	}
+	
+	std::ostringstream ss;
+	ss << file.rdbuf();
+
+	response.setStatus(200, "OK");
+	response.setBody(ss.str());
+	response.setHeader("Content-Type", "text/html");
+	return response;
+	
 }
 
 Response RequestHandler::handlePostMethod(const Request &request)
