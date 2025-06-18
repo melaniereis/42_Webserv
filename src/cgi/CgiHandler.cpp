6640b6497a7a0e1149cfcec66d1dc5ca82f173ae
Melanie Ferraz Reis <meferraz@c1r2s6.42porto.com>
2025-06-18 14:02:29 +0100

    improve cgiHandler files

--- src/cgi/CgiHandler.cpp
+++ src/cgi/CgiHandler.cpp
@@ -6,14 +6,24 @@
 /*   By: meferraz <meferraz@student.42porto.pt>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 11:33:32 by meferraz          #+#    #+#             */
-/*   Updated: 2025/06/16 14:32:10 by meferraz         ###   ########.fr       */
+/*   Updated: 2025/06/17 13:00:28 by meferraz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "CgiHandler.hpp"
 #include "../http/HttpStatus.hpp"
+#include <cstdlib>
+#include <cstring>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sstream>
+#include <fstream>
+#include <cctype>
 
-CgiHandler::CgiHandler(const Request &request, const ServerConfig &config, const LocationConfig &location)
+CgiHandler::CgiHandler(const Request &request,
+					const ServerConfig &config,
+					const LocationConfig &location)
 	: _request(request), _config(config), _location(location)
 {}
 
@@ -23,32 +33,49 @@ Response CgiHandler::execute()
 {
 	Response response;
 	std::string scriptPath = _resolveScriptPath();
-	std::cout << "Executing CGI script: " << scriptPath << std::endl;
+	Logger::info("Executing CGI script: " + scriptPath);
 
-	// Verify script exists and is executable
-	if (access(scriptPath.c_str(), F_OK)) {
-		return HttpStatus::buildResponse(response, 404);
-	}
-	if (access(scriptPath.c_str(), X_OK)) {
-		return HttpStatus::buildResponse(response, 403);
+	if (!_validateScript(scriptPath, response)) {
+		return response;
 	}
 
-	// Prepare environment
 	_initEnv();
-
-	// Execute CGI
 	std::string output = _executeCgiScript(scriptPath);
 	if (output.empty()) {
-		return HttpStatus::buildResponse(response, 500);
+		_handleCgiError(response);
+		return response;
 	}
 
-	// Parse CGI output
 	_parseCgiOutput(output, response);
 	return response;
 }
-std::string CgiHandler::_resolveScriptPath()
+
+bool CgiHandler::_validateScript(const std::string& scriptPath,
+								Response& response)
 {
-	// Use location's root if available, otherwise server root
+	if (access(scriptPath.c_str(), F_OK) != 0) {
+		int err = errno;
+		Logger::error("Script not found: " + scriptPath + " - " + strerror(err));
+		HttpStatus::buildResponse(response, 404);
+		return false;
+	}
+	if (access(scriptPath.c_str(), X_OK) != 0) {
+		int err = errno;
+		Logger::error("Script not executable: " + scriptPath + " - " + strerror(err));
+		HttpStatus::buildResponse(response, 403);
+		return false;
+	}
+	return true;
+}
+
+void CgiHandler::_handleCgiError(Response& response)
+{
+	Logger::error("CGI execution failed");
+	HttpStatus::buildResponse(response, 500);
+}
+
+std::string CgiHandler::_resolveScriptPath() const
+{
 	std::string base = _location.getRoot();
 	if (base.empty()) {
 		base = _config.getServerRoot();
@@ -57,24 +84,26 @@ std::string CgiHandler::_resolveScriptPath()
 	std::string path = _request.getReqPath();
 	std::string locPath = _location.getPath();
 
-	// Remove location prefix from request path
 	if (path.find(locPath) == 0) {
 		path = path.substr(locPath.length());
 	}
 
-	// Properly join paths with directory separator
-	if (base[base.length()-1] == '/' && !path.empty() && path[0] == '/') {
-		return base + path.substr(1);
-	} else if (base[base.length()-1] != '/' && !path.empty() && path[0] != '/') {
-		return base + "/" + path;
-	} else {
+	if (base[base.length()-1] == '/') {
+		if (!path.empty() && path[0] == '/') {
+			return base + path.substr(1);
+		}
+		return base + path;
+	}
+	else {
+		if (!path.empty() && path[0] != '/') {
+			return base + "/" + path;
+		}
 		return base + path;
 	}
 }
 
 void CgiHandler::_initEnv()
 {
-	// Set required CGI environment variables
 	_env["QUERY_STRING"] = _request.getReqQueryString();
 	_env["REQUEST_METHOD"] = _request.getReqMethod();
 	_env["SCRIPT_NAME"] = _request.getReqPath();
@@ -89,109 +118,171 @@ void CgiHandler::_initEnv()
 	_env["PATH_INFO"] = _request.getReqPath();
 }
 
+char** CgiHandler::_createEnvArray() const
+{
+	char** env = new char*[_env.size() + 1];
+	int i = 0;
+	for (std::map<std::string, std::string>::const_iterator it = _env.begin();
+		it != _env.end(); ++it)
+	{
+		std::string envVar = it->first + "=" + it->second;
+		env[i] = new char[envVar.length() + 1];
+		std::strcpy(env[i], envVar.c_str());
+		i++;
+	}
+	env[i] = NULL;
+	return env;
+}
+
+void CgiHandler::_cleanupEnvArray(char** env) const
+{
+	for (int i = 0; env[i]; i++) {
+		delete[] env[i];
+	}
+	delete[] env;
+}
+
+bool CgiHandler::_setupPipes(int pipeIn[2], int pipeOut[2])
+{
+	if (pipe(pipeIn) < 0) {
+		int err = errno;
+		Logger::error("pipeIn failed: " + std::string(strerror(err)));
+		return false;
+	}
+	if (pipe(pipeOut) < 0) {
+		int err = errno;
+		Logger::error("pipeOut failed: " + std::string(strerror(err)));
+		close(pipeIn[0]);
+		close(pipeIn[1]);
+		return false;
+	}
+	return true;
+}
+
+void CgiHandler::_childProcess(int pipeIn[2], int pipeOut[2],
+							const std::string& scriptPath)
+{
+	close(pipeIn[1]);
+	close(pipeOut[0]);
+
+	if (dup2(pipeIn[0], STDIN_FILENO) < 0) {
+		perror("dup2 stdin failed");
+		exit(EXIT_FAILURE);
+	}
+	if (dup2(pipeOut[1], STDOUT_FILENO) < 0) {
+		perror("dup2 stdout failed");
+		exit(EXIT_FAILURE);
+	}
+
+	char** env = _createEnvArray();
+	std::map<std::string, std::string> cgiMap = _location.getCgis();
+	std::string interpreter;
+	size_t dotPos = scriptPath.find_last_of('.');
+
+	if (dotPos != std::string::npos) {
+		std::string ext = scriptPath.substr(dotPos);
+		std::map<std::string, std::string>::const_iterator it = cgiMap.find(ext);
+		if (it != cgiMap.end()) {
+			interpreter = it->second;
+		}
+	}
+
+	char* argv[3] = { NULL, NULL, NULL };
+	if (!interpreter.empty()) {
+		argv[0] = const_cast<char*>(interpreter.c_str());
+		argv[1] = const_cast<char*>(scriptPath.c_str());
+	} else {
+		argv[0] = const_cast<char*>(scriptPath.c_str());
+	}
+
+	execve(argv[0], argv, env);
+	perror("execve failed");
+	_cleanupEnvArray(env);
+	exit(EXIT_FAILURE);
+}
+
+void CgiHandler::_parentProcess(int pipeIn[2], int pipeOut[2],
+							pid_t pid, std::string& output)
+{
+	close(pipeIn[0]);
+	close(pipeOut[1]);
+
+	const std::string& body = _request.getReqBody();
+	if (!body.empty()) {
+		ssize_t bytesWritten = write(pipeIn[1], body.c_str(), body.size());
+		if (bytesWritten < 0) {
+			int err = errno;
+			Logger::error("write to CGI failed: " + std::string(strerror(err)));
+		}
+	}
+	close(pipeIn[1]);
+
+	char buffer[4096];
+	ssize_t bytesRead;
+	while ((bytesRead = read(pipeOut[0], buffer, sizeof(buffer) - 1)) > 0) {
+		buffer[bytesRead] = '\0';
+		output.append(buffer, bytesRead);
+	}
+	close(pipeOut[0]);
+
+	int status;
+	waitpid(pid, &status, 0);
+	if (WIFEXITED(status)) {
+		if (WEXITSTATUS(status) != EXIT_SUCCESS) {
+			Logger::error("CGI script exited with status: " +
+						_intToString(WEXITSTATUS(status)));
+			output.clear();
+		}
+	} else {
+		Logger::error("CGI script did not exit normally");
+		output.clear();
+	}
+}
+
 std::string CgiHandler::_executeCgiScript(const std::string& scriptPath)
 {
 	int pipeIn[2], pipeOut[2];
 	pid_t pid;
 	std::string output;
 
-	if (pipe(pipeIn) || pipe(pipeOut)) {
+	if (!_setupPipes(pipeIn, pipeOut)) {
 		return "";
 	}
 
 	pid = fork();
-	if (pid == -1) {
+	if (pid < 0) {
+		int err = errno;
+		Logger::error("fork failed: " + std::string(strerror(err)));
 		close(pipeIn[0]); close(pipeIn[1]);
 		close(pipeOut[0]); close(pipeOut[1]);
 		return "";
 	}
 
-	if (pid == 0) { // Child process
-		close(pipeIn[1]);
-		close(pipeOut[0]);
-
-		dup2(pipeIn[0], STDIN_FILENO);
-		dup2(pipeOut[1], STDOUT_FILENO);
-
-		// Convert environment map to char **
-		char **env = new char*[_env.size() + 1];
-		int i = 0;
-		for (std::map<std::string, std::string>::iterator it = _env.begin();
-			it != _env.end(); ++it)
-		{
-			std::string envVar = it->first + "=" + it->second;
-			env[i] = new char[envVar.length() + 1];
-			strcpy(env[i], envVar.c_str());
-			i++;
-		}
-		env[i] = NULL;
-
-		// Get interpreter for script extension
-		std::map<std::string, std::string> cgiMap = _location.getCgis();
-		size_t dotPos = scriptPath.find_last_of('.');
-		std::string interpreter;
-		if (dotPos != std::string::npos) {
-			std::string ext = scriptPath.substr(dotPos);
-			if (cgiMap.find(ext) != cgiMap.end()) {
-				interpreter = cgiMap[ext];
-			}
-		}
-
-		char *argv[3] = { NULL, NULL, NULL };
-		if (!interpreter.empty()) {
-			argv[0] = const_cast<char*>(interpreter.c_str());
-			argv[1] = const_cast<char*>(scriptPath.c_str());
-		} else {
-			argv[0] = const_cast<char*>(scriptPath.c_str());
-		}
-
-		execve(argv[0], argv, env);
-
-		// Cleanup if execve fails
-		for (int j = 0; env[j]; j++) {
-			delete[] env[j];
-		}
-		delete[] env;
-		exit(1);
-	} else { // Parent process
-		close(pipeIn[0]);
-		close(pipeOut[1]);
-
-		// Write request body to CGI stdin
-		if (!_request.getReqBody().empty()) {
-			write(pipeIn[1], _request.getReqBody().c_str(), _request.getReqBody().size());
-		}
-		close(pipeIn[1]);
-
-		// Read CGI output
-		char buffer[4096];
-		ssize_t bytesRead;
-		while ((bytesRead = read(pipeOut[0], buffer, sizeof(buffer)-1)) > 0) {
-			buffer[bytesRead] = '\0';
-			output.append(buffer);
-		}
-		close(pipeOut[0]);
-
-		// Wait for child to finish
-		int status;
-		waitpid(pid, &status, 0);
-		if (WIFEXITED(status)) {
-			if (WEXITSTATUS(status)) {
-				return "";
-			}
-		} else {
-			return "";
-		}
+	if (pid == 0) {
+		_childProcess(pipeIn, pipeOut, scriptPath);
+	} else {
+		_parentProcess(pipeIn, pipeOut, pid, output);
 	}
-
 	return output;
+}
+
+static std::string trim(const std::string& str)
+{
+	size_t first = str.find_first_not_of(" \t\r\n");
+	if (first == std::string::npos) {
+		return "";
+	}
+	size_t last = str.find_last_not_of(" \t\r\n");
+	return str.substr(first, (last - first + 1));
 }
 
 void CgiHandler::_parseCgiOutput(const std::string& output, Response& response)
 {
 	size_t headerEnd = output.find("\r\n\r\n");
+	bool doubleNewline = false;
 	if (headerEnd == std::string::npos) {
 		headerEnd = output.find("\n\n");
+		doubleNewline = true;
 		if (headerEnd == std::string::npos) {
 			response.setBody(output);
 			response.setStatus(200, "OK");
@@ -200,35 +291,36 @@ void CgiHandler::_parseCgiOutput(const std::string& output, Response& response)
 	}
 
 	std::string headers = output.substr(0, headerEnd);
-	std::string body = output.substr(headerEnd + 4);
-
+	std::string body = output.substr(headerEnd + (doubleNewline ? 2 : 4));
 	response.setBody(body);
 
-	// Parse headers
 	std::istringstream headerStream(headers);
 	std::string headerLine;
 	while (std::getline(headerStream, headerLine)) {
-		size_t colonPos = headerLine.find(':');
-		if (colonPos != std::string::npos) {
-			std::string key = headerLine.substr(0, colonPos);
-			std::string value = headerLine.substr(colonPos + 1);
-			// Trim whitespace
-			key.erase(key.find_last_not_of(" \t\r\n") + 1);
-			value.erase(0, value.find_first_not_of(" \t"));
-			value.erase(value.find_last_not_of(" \t\r\n") + 1);
-			response.setHeader(key, value);
-		} else if (!headerLine.empty()) {
-			// Handle status line
-			if (headerLine.substr(0, 7) == "Status:") {
-				std::string status = headerLine.substr(7);
-				status.erase(0, status.find_first_not_of(" \t"));
-				size_t spacePos = status.find(' ');
-				if (spacePos != std::string::npos) {
-					response.setStatus(std::atoi(status.substr(0, spacePos).c_str()),
-								status.substr(spacePos+1));
-				} else {
-					response.setStatus(std::atoi(status.c_str()), "OK");
-				}
+		if (headerLine.empty()) continue;
+		if (headerLine[headerLine.size()-1] == '\r') {
+			headerLine.erase(headerLine.size()-1);
+		}
+
+		if (headerLine.find("Status:") == 0) {
+			std::string status = headerLine.substr(7);
+			status = trim(status);
+			size_t spacePos = status.find(' ');
+			if (spacePos != std::string::npos) {
+				std::string code = status.substr(0, spacePos);
+				std::string message = status.substr(spacePos + 1);
+				response.setStatus(atoi(code.c_str()), message);
+			} else {
+				response.setStatus(atoi(status.c_str()), "OK");
+			}
+		}
+		else {
+			size_t colonPos = headerLine.find(':');
+			if (colonPos != std::string::npos) {
+				std::string key = headerLine.substr(0, colonPos);
+				std::string value = headerLine.substr(colonPos + 1);
+				value = trim(value);
+				response.setHeader(key, value);
 			}
 		}
 	}
--- src/cgi/CgiHandler.hpp
+++ src/cgi/CgiHandler.hpp
@@ -6,7 +6,7 @@
 /*   By: meferraz <meferraz@student.42porto.pt>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 11:28:41 by meferraz          #+#    #+#             */
-/*   Updated: 2025/06/16 14:30:59 by meferraz         ###   ########.fr       */
+/*   Updated: 2025/06/17 12:30:23 by meferraz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,25 +17,40 @@
 #include "../config/ServerConfig.hpp"
 #include "../config/LocationConfig.hpp"
 #include "../utils/Logger.hpp"
-
-class HttpStatus;
+#include <map>
 
 class CgiHandler
 {
 	public:
-		CgiHandler(const Request &request, const ServerConfig &config, const LocationConfig &location);
+		CgiHandler(const Request &request,
+				const ServerConfig &config,
+				const LocationConfig &location);
 		~CgiHandler();
 
 		Response execute();
+
 	private:
-		std::string _resolveScriptPath();
+		Request _request;
+		ServerConfig _config;
+		LocationConfig _location;
+		std::map<std::string, std::string> _env;
+
+		std::string _resolveScriptPath() const;
 		void _initEnv();
 		std::string _executeCgiScript(const std::string &scriptPath);
 		void _parseCgiOutput(const std::string &output, Response &response);
 		std::string _intToString(int value) const;
-
-		Request _request;
-		ServerConfig _config;
-		LocationConfig _location;
-		std::map<std::string, std::string> _env; // Environment variables for CGI
+		char** _createEnvArray() const;
+		void _cleanupEnvArray(char** env) const;
+		bool _setupPipes(int pipeIn[2], int pipeOut[2]) const;
+		void _childProcess(int pipeIn[2], int pipeOut[2],
+						const std::string& scriptPath);
+		void _parentProcess(int pipeIn[2], int pipeOut[2],
+						pid_t pid, std::string& output);
+		bool _validateScript(const std::string& scriptPath, Response& response);
+		void _handleCgiError(Response& response);
+		bool _setupPipes(int pipeIn[2], int pipeOut[2]);
+		CgiHandler();
+		CgiHandler(const CgiHandler&);
+		CgiHandler& operator=(const CgiHandler&);
 };
